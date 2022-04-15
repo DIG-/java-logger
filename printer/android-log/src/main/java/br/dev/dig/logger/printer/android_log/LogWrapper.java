bdc8e29264a458b4150dd1dc75ac2153516819e5
Gustavo de Oliveira Silva (DIG) <diguser@gmail.com>
2022-04-15 09:20:13 -0300

    Dummy test for AndroidLog - LogWrapper

--- printer/android-log/src/main/java/br/dev/dig/logger/printer/android_log/LogWrapper.java
+++ printer/android-log/src/main/java/br/dev/dig/logger/printer/android_log/LogWrapper.java
@@ -2,28 +2,31 @@ package br.dev.dig.logger.printer.android_log;
 
 import android.util.Log;
 
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
 class LogWrapper {
-    void v(String tag, CharSequence msg, Throwable tr) {
+    void v(@Nullable final String tag, @NotNull final CharSequence msg, @Nullable final Throwable tr) {
         Log.v(tag, msg.toString(), tr);
     }
 
-    void d(String tag, CharSequence msg, Throwable tr) {
+    void d(@Nullable final String tag, @NotNull final CharSequence msg, @Nullable final Throwable tr) {
         Log.d(tag, msg.toString(), tr);
     }
 
-    void i(String tag, CharSequence msg, Throwable tr) {
+    void i(@Nullable final String tag, @NotNull final CharSequence msg, @Nullable final Throwable tr) {
         Log.i(tag, msg.toString(), tr);
     }
 
-    void w(String tag, CharSequence msg, Throwable tr) {
+    void w(@Nullable final String tag, @NotNull final CharSequence msg, @Nullable final Throwable tr) {
         Log.w(tag, msg.toString(), tr);
     }
 
-    void e(String tag, CharSequence msg, Throwable tr) {
+    void e(@Nullable final String tag, @NotNull final CharSequence msg, @Nullable final Throwable tr) {
         Log.e(tag, msg.toString(), tr);
     }
 
-    void wtf(String tag, CharSequence msg, Throwable tr) {
+    void wtf(@Nullable final String tag, @NotNull final CharSequence msg, @Nullable final Throwable tr) {
         Log.wtf(tag, msg.toString(), tr);
     }
 }
