7938bdf1f9e5dfb60041786b9e2f21863aaab338
Gustavo de Oliveira Silva (DIG) <diguser@gmail.com>
2022-04-24 23:27:53 -0300

    Updated creation method of SystemLogLogger into Builder pattern

--- printer/system-log/src/main/java/br/dev/dig/logger/printer/system/SystemLogLogger.java
+++ printer/system-log/src/main/java/br/dev/dig/logger/printer/system/SystemLogLogger.java
@@ -1,15 +1,51 @@
 package br.dev.dig.logger.printer.system;
 
+import com.sun.jna.Platform;
+
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import br.dev.dig.logger.BaseLogger;
+import br.dev.dig.logger.printer.println.PrintlnFormatter;
 import br.dev.dig.logger.printer.stub.StubLogger;
+import br.dev.dig.logger.printer.system.windows.WindowsLogLogger;
 
 public abstract class SystemLogLogger implements BaseLogger {
 
-    public static BaseLogger create(@NotNull final String applicationName) {
-        return new StubLogger();
+    @SuppressWarnings("unused")
+    public static class Builder {
+        @Nullable
+        String appName;
+        @NotNull
+        PrintlnFormatter windowsFormatter = WindowsLogLogger.Formatter.simple();
+
+        @Nullable
+        public String getAppName() {
+            return appName;
+        }
+
+        public Builder setAppName(@NotNull final String appName) {
+            this.appName = appName;
+            return this;
+        }
+
+        @NotNull
+        public PrintlnFormatter getWindowsFormatter() {
+            return windowsFormatter;
+        }
+
+        public Builder setWindowsFormatter(@NotNull final PrintlnFormatter windowsFormatter) {
+            this.windowsFormatter = windowsFormatter;
+            return this;
+        }
+
+        @SuppressWarnings("ConstantConditions")
+        public BaseLogger build() {
+            if (Platform.isWindows()) {
+                return new WindowsLogLogger(appName, windowsFormatter);
+            }
+            return new StubLogger();
+        }
     }
 
     @Override
