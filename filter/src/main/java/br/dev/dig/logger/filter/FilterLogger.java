a2dccb58667f490bc9d02e16a334a022f6eccb11
Gustavo de Oliveira Silva (DIG) <diguser@gmail.com>
2022-04-25 10:45:18 -0300

    Changed parent to target in FilterLogger

--- filter/src/main/java/br/dev/dig/logger/filter/FilterLogger.java
+++ filter/src/main/java/br/dev/dig/logger/filter/FilterLogger.java
@@ -10,32 +10,32 @@ import br.dev.dig.logger.Logger;
 
 public final class FilterLogger implements BaseLogger {
     @NotNull
-    final BaseLogger parent;
+    final BaseLogger target;
     final int level;
 
     @SuppressWarnings("ConstantConditions")
-    public FilterLogger(int minimum, @NotNull final BaseLogger parent) {
+    public FilterLogger(int minimum, @NotNull final BaseLogger target) {
         if (minimum < Logger.LEVEL_VERBOSE || minimum > Logger.LEVEL_NONE) {
             throw new InvalidParameterException("Minimum must be between `Logger.LEVEL_VERBOSE` and `Logger.LEVEL_NONE`.");
         }
-        if (parent == null) {
-            throw new InvalidParameterException("Parent BaseLogger must not be null");
+        if (target == null) {
+            throw new InvalidParameterException("Target BaseLogger must not be null");
         }
         this.level = minimum;
-        this.parent = parent;
+        this.target = target;
     }
 
     @Override
     public void log(int level, @Nullable final String tag, @NotNull final Message message, @Nullable final Throwable throwable) {
         if (level >= this.level) {
-            parent.log(level, tag, message, throwable);
+            target.log(level, tag, message, throwable);
         }
     }
 
     @Override
     public void log(int level, @Nullable final String tag, @Nullable final CharSequence message, @Nullable final Throwable throwable) {
         if (level >= this.level) {
-            parent.log(level, tag, message, throwable);
+            target.log(level, tag, message, throwable);
         }
     }
 }
