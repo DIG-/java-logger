0921b13f484f46636fe103c7c19fc07cc961bcfe
Gustavo de Oliveira Silva (DIG) <diguser@gmail.com>
2022-04-25 10:42:51 -0300

    Added Conditions check on constructor of FilterLogger

--- filter/src/main/java/br/dev/dig/logger/filter/FilterLogger.java
+++ filter/src/main/java/br/dev/dig/logger/filter/FilterLogger.java
@@ -3,15 +3,24 @@ package br.dev.dig.logger.filter;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import java.security.InvalidParameterException;
+
 import br.dev.dig.logger.BaseLogger;
+import br.dev.dig.logger.Logger;
 
 public final class FilterLogger implements BaseLogger {
     @NotNull
     final BaseLogger parent;
     final int level;
 
+    @SuppressWarnings("ConstantConditions")
     public FilterLogger(int minimum, @NotNull final BaseLogger parent) {
-        super();
+        if (minimum < Logger.LEVEL_VERBOSE || minimum > Logger.LEVEL_NONE) {
+            throw new InvalidParameterException("Minimum must be between `Logger.LEVEL_VERBOSE` and `Logger.LEVEL_NONE`.");
+        }
+        if (parent == null) {
+            throw new InvalidParameterException("Parent BaseLogger must not be null");
+        }
         this.level = minimum;
         this.parent = parent;
     }
