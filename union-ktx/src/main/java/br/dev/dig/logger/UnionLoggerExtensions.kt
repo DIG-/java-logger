4d21d81effe8ee26d843248945a4f58346487ead
Gustavo de Oliveira Silva (DIG) <diguser@gmail.com>
2022-05-09 11:58:55 -0300

    Created union-ktx

---
+++ union-ktx/src/main/java/br/dev/dig/logger/UnionLoggerExtensions.kt
@@ -0,0 +1,11 @@
+package br.dev.dig.logger
+
+import br.dev.dig.logger.builder.LoggerBuilder
+import br.dev.dig.logger.union.UnionLogger
+
+@Suppress("unused", "FunctionName")
+inline fun LoggerBuilder.Union(crossinline config: UnionLogger.Builder.() -> Unit): UnionLogger {
+    val builder = UnionLogger.Builder()
+    config(builder)
+    return builder.build()
+}
