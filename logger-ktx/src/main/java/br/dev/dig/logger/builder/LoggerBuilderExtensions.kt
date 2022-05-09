9560bb766fd3120e4d16d4555f0908d430293d54
Gustavo de Oliveira Silva (DIG) <diguser@gmail.com>
2022-05-09 12:21:14 -0300

    Created extensions to get short/long logger

--- logger-ktx/src/main/java/br/dev/dig/logger/builder/LoggerBuilderExtensions.kt
+++ logger-ktx/src/main/java/br/dev/dig/logger/builder/LoggerBuilderExtensions.kt
@@ -3,6 +3,8 @@
 package br.dev.dig.logger.builder
 
 import br.dev.dig.logger.BaseLogger
+import br.dev.dig.logger.style.LongLogger
+import br.dev.dig.logger.style.SmallLogger
 
 inline fun LoggerBuilder(crossinline logger: LoggerBuilder.() -> BaseLogger): LoggerBuilder =
     object : LoggerBuilder() {
@@ -17,3 +19,17 @@ inline fun CachedLoggerBuilder(crossinline logger: CachedLoggerBuilder.() -> Bas
             return logger()
         }
     }
+
+@Suppress("NOTHING_TO_INLINE")
+inline fun <Builder : LoggerBuilder> Builder.getSmallLogger(tag: String? = null): SmallLogger =
+    getLogger(tag)
+
+val <Builder : LoggerBuilder> Builder.smallLogger: SmallLogger
+    inline get() = logger
+
+@Suppress("NOTHING_TO_INLINE")
+inline fun <Builder : LoggerBuilder> Builder.getLongLogger(tag: String? = null): LongLogger =
+    getLogger(tag)
+
+val <Builder : LoggerBuilder> Builder.longLogger: LongLogger
+    inline get() = logger
