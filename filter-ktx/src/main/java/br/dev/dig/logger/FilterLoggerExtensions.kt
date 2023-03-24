08a0f3c30d0f792c8e89b5e255f7794c5deee1da
Gustavo de Oliveira Silva (DIG) <diguser@gmail.com>
2023-03-24 10:06:00 -0300

    Adjust FilterLoggerExtensions to correct Builder return

--- filter-ktx/src/main/java/br/dev/dig/logger/FilterLoggerExtensions.kt
+++ filter-ktx/src/main/java/br/dev/dig/logger/FilterLoggerExtensions.kt
@@ -4,7 +4,7 @@ import br.dev.dig.logger.builder.LoggerBuilder
 import br.dev.dig.logger.filter.FilterLogger
 
 @Suppress("unused", "FunctionName")
-inline fun LoggerBuilder.FilterLogger(crossinline config: FilterLogger.Builder.() -> Unit): FilterLogger {
+inline fun LoggerBuilder.FilterLogger(crossinline config: FilterLogger.Builder.() -> Unit): BaseLogger {
     val builder = FilterLogger.Builder()
     config(builder)
     return builder.build()
